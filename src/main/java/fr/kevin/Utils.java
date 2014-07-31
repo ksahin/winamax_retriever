9852c411f86aaa3ccabbb1d37964606a8afc823d
ksahin <sahin.kevin@gmail.com>
2014-07-31 22:19:29 +0200

    Ajout methode qui convertie une date vers un unixtimestamp

--- src/main/java/fr/kevin/Utils.java
+++ src/main/java/fr/kevin/Utils.java
@@ -1,7 +1,25 @@
 package fr.kevin;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import fr.kevin.exception.InvalidDateFormatException;
+
 public class Utils {
-	public static String cleanAmount(String amount){
-		return amount.replace(" ", "").replace("€","").replace(",", ".");
+
+	public static String cleanAmount(String amount) {
+		return amount.replace(" ", "").replace("€", "").replace(",", ".");
+	}
+
+	public static String dateToUnixTimeStamp(String date)
+			throws InvalidDateFormatException {
+		Date date2 = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			date2 = formatter.parse(date);
+		} catch (Exception e) {
+			throw new InvalidDateFormatException(date);
+		}
+		return String.valueOf((date2.getTime() / 1000));
 	}
 }
