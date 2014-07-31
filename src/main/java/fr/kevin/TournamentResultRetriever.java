555b801ce0148980a9b68fe6cbc4bfa35bdaf3c7
ksahin <sahin.kevin@gmail.com>
2014-07-31 12:38:00 +0200

    Ajout methode getTournamentResult

--- src/main/java/fr/kevin/TournamentResultRetriever.java
+++ src/main/java/fr/kevin/TournamentResultRetriever.java
@@ -1,24 +1,42 @@
 package fr.kevin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
 
 public class TournamentResultRetriever {
-	public static void main(String args[]) throws Exception {
-
+	
+	public static List<TournamentResult> getTournementResult(int tournamentId) throws Exception{
 		WebClient client = new WebClient();
 		client.getOptions().setJavaScriptEnabled(false);
 		client.getOptions().setCssEnabled(false);
-		String tournamentId = "82915467";
 		HtmlPage page = client.getPage("https://www.winamax.fr/poker/tournament.php?ID="+tournamentId);
 		HtmlTable table = page.getFirstByXPath("//table[@class='ranking']");
+		List<TournamentResult> resultList = new ArrayList<TournamentResult>();
 		for(HtmlTableRow row : table.getBodies().get(0).getRows()){
 			//Ici il faudra check que le player existe en base ou pas
-			PlayerBean player = new PlayerBean();
-			player.setPseudo(row.getChildNodes().get(1).asText());
-			
+			TournamentResult result = new TournamentResult();
+			result.setPseudoPlayer(row.getChildNodes().get(1).asText());
+			result.setTournamentId(tournamentId);
+			result.setAmountWon(Double.valueOf(fr.kevin.Utils.cleanAmount(row.getChildNodes().get(2).asText())));
+			result.setRank(Integer.valueOf(row.getChildNodes().get(0).asText()));
+			resultList.add(result);
 		}
+		return resultList;
+	}
+	public static void main(String args[]) throws Exception {
+
+		List<TournamentResult> listResult = getTournementResult(82915467);
+		for(TournamentResult res : listResult){
+			System.out.println("Id du tournois : "+ res.getTournamentId());
+			System.out.println("Pseudo joueur : "+ res.getPseudoPlayer());
+			System.out.println("Gain  "+ res.getAmountWon());
+			System.out.println("Rank : "+ res.getRank());
+		}
+		
 	}
 }
